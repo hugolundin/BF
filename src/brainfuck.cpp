1beb49f9bcb550dd1505769580ede50d5dff8ea4
Hugo Lundin <hugo@lundin.dev>
2021-06-26 22:05:47 +0200

    Implement jumps

--- src/brainfuck.cpp
+++ src/brainfuck.cpp
@@ -2,54 +2,79 @@
 #include <iostream>
 #include <stdexcept>
 #include <utility>
+#include <stack>
 
 #include "brainfuck.h"
 
 Brainfuck::Brainfuck(std::string tokens):
-    m_pointer(0), m_pc(0), m_tokens(std::move(tokens))
+    m_pointer(0), m_pc(0),
+    m_tokens(std::move(tokens)), m_tape(), m_jumps()
 {
     m_tape.fill(0);
+
+    int start;
+    char current;
+    std::stack<int> braces;
+
+    for (int position = 0; position < m_tokens.size(); position++) {
+        current = m_tokens[position];
+
+        if (current == TOKEN_JUMP_IF_ZERO) {
+            braces.push(position);
+        } else if (current == TOKEN_JUMP_IF_NOT_ZERO) {
+            start = braces.top();
+            braces.pop();
+            m_jumps[start] = position;
+            m_jumps[position] = start;
+        }
+    }
 }
 
 Brainfuck::~Brainfuck() = default;
 
+void Brainfuck::step()
+{
+    switch (m_tokens[m_pc]) {
+        case TOKEN_MOVE_RIGHT:
+            move_right();
+            break;
+        case TOKEN_MOVE_LEFT:
+            move_left();
+            break;
+        case TOKEN_INCREMENT:
+            increment();
+            break;
+        case TOKEN_DECREMENT:
+            decrement();
+            break;
+        case TOKEN_OUTPUT:
+            print();
+            break;
+        case TOKEN_INPUT:
+            read();
+            break;
+        case TOKEN_JUMP_IF_ZERO:
+            jump_if_zero();
+            break;
+        case TOKEN_JUMP_IF_NOT_ZERO:
+            jump_if_not_zero();
+            break;
+        default:
+            break;
+    }
+
+    m_pc++;
+}
+
 void Brainfuck::run()
 {
     while (!finished()) {
-        switch (m_tokens[m_pc]) {
-            case '>':
-                move_right();
-                break;
-            case '<':
-                move_left();
-                break;
-            case '+':
-                increment();
-                break;
-            case '-':
-                decrement();
-                break;
-            case '.':
-                print();
-                break;
-            case ',':
-                read();
-                break;
-            case '[':
-                jump_if_zero();
-                break;
-            case ']':
-                jump_if_not_zero();
-                break;
-            default: {
-                std::stringstream ss;
-                ss << "unknown token: " << "'" << m_tokens[m_pc] << "'";
-                throw std::runtime_error(ss.str());
-            }
-        }
-
-        m_pc++;
+        step();
     }
+}
+
+bool Brainfuck::finished() {
+    return m_pc >= m_tokens.size();
 }
 
 void Brainfuck::move_right()
@@ -86,14 +111,14 @@ void Brainfuck::read()
 void Brainfuck::jump_if_zero()
 {
     if (m_tape[m_pointer] == 0) {
-        proceed_until(']', true);
+        m_pc = m_jumps[m_pc];
     }
 }
 
 void Brainfuck::jump_if_not_zero()
 {
     if (m_tape[m_pointer] != 0) {
-        proceed_until('[', false);
+        m_pc = m_jumps[m_pc];
     }
 }
 
@@ -103,7 +128,7 @@ std::ostream& operator<<(std::ostream& out, const Brainfuck& b)
     out << "[";
 
     std::string delimiter;
-    for (int i = 0; i < TAPE_SIZE; i++) {
+    for (int i = 0; i < BF_TAPE_SIZE; i++) {
         out << delimiter << b.m_tape[i];
         delimiter = ", ";
     }
@@ -112,18 +137,4 @@ std::ostream& operator<<(std::ostream& out, const Brainfuck& b)
     return out;
 }
 
-void Brainfuck::proceed_until(char c, bool direction)
-{
-    while (!finished() && !match(c)) {
-        m_pc += direction ? 1 : -1;
-    }
-}
-
-bool Brainfuck::finished() {
-    return m_pc >= m_tokens.size();
-}
-
-bool Brainfuck::match(char c) {
-    return c == m_tokens[m_pc];
-}
 
--- src/brainfuck.h
+++ src/brainfuck.h
@@ -1,26 +1,37 @@
 #pragma once
 
+#include <map>
 #include <array>
 #include <string>
 
-const int TAPE_SIZE = 256;
+#define TOKEN_MOVE_RIGHT '>'
+#define TOKEN_MOVE_LEFT '<'
+#define TOKEN_INCREMENT '+'
+#define TOKEN_DECREMENT '-'
+#define TOKEN_OUTPUT '.'
+#define TOKEN_INPUT ','
+#define TOKEN_JUMP_IF_ZERO '['
+#define TOKEN_JUMP_IF_NOT_ZERO ']'
+
+const int BF_TAPE_SIZE = 256;
 
 class Brainfuck {
     public:
         explicit Brainfuck(std::string);
         ~Brainfuck();
+        
+        void step();
         void run();
+        bool finished();
+
         friend std::ostream& operator<<(std::ostream&, const Brainfuck&);
 
     private:
         int m_pc;
         int m_pointer;
         std::string m_tokens;
-        std::array<int, TAPE_SIZE> m_tape {};
-
-        bool finished();
-        bool match(char);
-        void proceed_until(char, bool);
+        std::map<int, int> m_jumps;
+        std::array<int, BF_TAPE_SIZE> m_tape;
 
         void move_right();
         void move_left();
